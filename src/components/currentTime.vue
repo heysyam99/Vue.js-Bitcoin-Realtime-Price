c9ddd86acab0e3908d40a22b99ff8e999dc11d39
heysyam99 <35216709+heysyam99@users.noreply.github.com>
2019-10-20 22:19:31 +0800

    add (currentTime) - Add clock into the app

---
+++ src/components/currentTime.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{ jun }}</h3>
+  </div>
+</template>
+
+<script>
+import moment from "moment-timezone";
+
+export default {
+  name: "currTime",
+  data() {
+    return {
+      jun: ""
+    };
+  },
+  created() {
+    this.liveTime();
+  },
+  methods: {
+    liveTime() {
+      setInterval(() => {
+        this.jun = moment.tz(moment.tz.guess()).format("hh:mm:ss A Z");
+      }, 100);
+    }
+  }
+};
+</script>
